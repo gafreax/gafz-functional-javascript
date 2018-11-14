824169772ec5960e2bd5e2f7a3e552fb63f79042
Gabriele Fontana <gabriele.fontana@mondora.com>
2018-11-14 15:59:44 +0100

    [feat]: add es6

--- es6.js
+++ es6.js
@@ -1,6 +1,9 @@
-
 function countWords(inputWords) {
-
+  var c = inputWords.reduce((acc, val) => {
+    acc[val] = (acc[val] || 0) + 1;
+    return acc;
+  }, {});
+  return c;
 }
 
 module.exports = countWords;
